72955a641d44b2122df372d8e265f1b0c6784598
Student <example@example.com>
2019-08-27 08:20:03 -0700

    add user methods and update migration for location from integer to decimal

--- db/migrate/20190826171923_add_locations_table.rb
+++ db/migrate/20190826171923_add_locations_table.rb
@@ -2,8 +2,8 @@ class AddLocationsTable < ActiveRecord::Migration[6.0]
   def change
     create_table :locations do |t|
       t.column :thing_id, :integer
-      t.column :lat, :integer
-      t.column :long, :integer
+      t.column :lat, :decimal
+      t.column :long, :decimal
     end
   end
 end
